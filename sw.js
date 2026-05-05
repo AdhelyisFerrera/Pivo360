1ec19ad7d27b80cfdc0461e0445bd9b9b9d29625
AdhelyisFerrera <adhelyis.ferrera@gmail.com>
2026-05-05 01:23:49 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-const CACHE_NAME = 'pasantia-tracker-v1';
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
