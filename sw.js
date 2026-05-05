1c284880a2e701a736cd103883f5797e4ed84616
AdhelyisFerrera <adhelyis.ferrera@gmail.com>
2026-05-05 12:01:14 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,29 @@
-const CACHE_NAME = 'pasantia-tracker-v1';
-const CACHE_NAME = 'pivo360-v1';
+const CACHE_NAME = 'pivo360-v2';
 
 const FILES = [
-  './',
-  './index.html',
-  './manifest.json'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
   );
 });
 
